9021113511cecf75968425d1b32b9c4442e1fd55
EzeZungri99 <zungri2020@gmail.com>
2023-07-19 11:49:29 -0300

    h

--- DualLanding/duallanding/landingdual/app/components/Secciones/contenidoSeccion/Portfolio/portfolio.jsx
+++ DualLanding/duallanding/landingdual/app/components/Secciones/contenidoSeccion/Portfolio/portfolio.jsx
@@ -2,41 +2,126 @@ import React from "react";
 import Image from "next/image";
 
 export default function Portfolio() {
+  const containerStyle = {
+    padding: "2rem 0",
+    paddingLeft: "8rem", // lg:px-32
+    paddingTop: "4rem", // lg:pt-12
+  };
+
+  const imageContainerStyle = {
+    margin: "-0.25rem", // -m-1
+    display: "flex",
+    flexWrap: "wrap",
+    margin: "-0.5rem", // md:-m-2
+  };
+
+  const imageWrapperStyle = {
+    width: "50%", // w-1/2
+    flexWrap: "wrap",
+  };
+
+  const imageStyle = {
+    width: "100%", // w-full
+    height: "100%", // h-full
+    borderRadius: "0.25rem", // rounded-lg
+    objectFit: "cover", // object-cover
+    objectPosition: "center", // object-center
+  };
+
+  const miImagenStyle = {
+    ...imageStyle, // Reutiliza los estilos de imageStyle
+    border: "5px solid #000", // Por ejemplo, el borde definido en portfolio.css
+    // ... Otros estilos personalizados de miImagen
+  };
+
+  const miImagenStyle2 = {
+    ...imageStyle,
+    // ... Estilos personalizados para miImagen2 (si los tienes)
+  };
+
+  const animationBoxStyle = {
+    fontSize: "3vw",
+    margin: "max(1rem, 3vw)",
+    border: "0.35rem solid",
+    padding: "3vw",
+    animation: "borderRotate var(--d) linear infinite forwards",
+  };
+
+  const animationBoxStyle2 = {
+    ...animationBoxStyle,
+    animation: "borderRadial var(--d) linear infinite forwards",
+  };
+
   return (
-    <div class="container  py-2 lg:px-32 lg:pt-12">
-      <div class="-m-1 flex flex-wrap md:-m-2">
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-full p-1 md:p-2">
+    <div style={containerStyle}>
+      <style jsx>{`
+        @import url("https://fonts.googleapis.com/css?family=Raleway:400");
+
+        @keyframes borderRotate {
+          100% {
+            --angle: 420deg;
+          }
+        }
+        
+        @keyframes borderRadial {
+          20% {
+            --gradX: 100%;
+            --gradY: 50%;
+          }
+          40% {
+            --gradX: 100%;
+            --gradY: 100%;
+          }
+          60% {
+            --gradX: 50%;
+            --gradY: 100%;
+          }
+          80% {
+            --gradX: 0%;
+            --gradY: 50%;
+          }
+          100% {
+            --gradX: 50%;
+            --gradY: 0%;
+          }
+        }
+        
+        /* Estilos adicionales de tu componente */
+        /* Por ejemplo, si necesitas estilos específicos para el componente */
+      `}</style>
+      <div style={imageContainerStyle}>
+        <div style={imageWrapperStyle}>
+          <div style={{ padding: "0.5rem", ...animationBoxStyle }}>
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              style={{ ...imageStyle, ...miImagenStyle }}
               src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
             />
           </div>
         </div>
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-full p-1 md:p-2">
+        <div style={imageWrapperStyle}>
+          <div style={{ padding: "0.5rem", ...animationBoxStyle }}>
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              style={{ ...imageStyle, ...miImagenStyle }}
               src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
             />
           </div>
         </div>
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-full p-1 md:p-2">
+        <div style={imageWrapperStyle}>
+          <div style={{ padding: "0.5rem", ...animationBoxStyle }}>
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              style={{ ...imageStyle, ...miImagenStyle2 }}
               src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
             />
           </div>
         </div>{" "}
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-full p-1 md:p-2">
+        <div style={imageWrapperStyle}>
+          <div style={{ padding: "0.5rem", ...animationBoxStyle }}>
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              style={{ ...imageStyle, ...miImagenStyle2 }}
               src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
             />
           </div>
